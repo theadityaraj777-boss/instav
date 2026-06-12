4dc5c67d244be1d11ce9ac6fefb4ea77ed900ca7
theadityaraj777-boss <theadityaraj777@gmail.com>
2026-06-13 01:33:40 +0530

    Update main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,4 +1,4 @@
-//import { InternetIdentityProvider } from "@caffeineai/core-infrastructure";
+import { InternetIdentityProvider } from "@caffeineai/core-infrastructure";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -18,8 +18,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-  //  <InternetIdentityProvider>
+    <InternetIdentityProvider>
       <App />
-   // </InternetIdentityProvider>
+    </InternetIdentityProvider>
   </QueryClientProvider>,
 );
